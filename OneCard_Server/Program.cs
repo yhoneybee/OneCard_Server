38a24ae290cf9e863b6ade9a8cc5785ec22514ea
chae young hoon <chaeyh04@gmail.com>
2021-04-22 02:41:05 +0900

    Init

---
+++ OneCard_Server/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Nettention.Proud;
+
+namespace OneCard_Server
+{
+    public class Room
+    {
+        public Room(HostID owner, string name, int pw, int max_player)
+        {
+            this.owner = owner;
+            this.name = name;
+            this.pw = pw;
+            this.max_player = max_player;
+        }
+        public bool Join(HostID ID)
+        {
+            if (hostIDs.Count < max_player)
+            {
+                hostIDs.Add(ID);
+                return true;
+            }
+            return false;
+        }
+        public bool Leave(HostID ID)
+        {
+            for (int i = 0; i < hostIDs.Count; i++)
+            {
+                if (hostIDs[i] == ID)
+                {
+                    hostIDs.RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public HostID owner;
+        public string name;
+        public int pw;
+        public int max_player;
+        public FastArray<HostID> hostIDs = new FastArray<HostID>();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetServer server = new NetServer();
+        }
+    }
+}
---
+++ PIDL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIDL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
